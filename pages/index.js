693fce8aab6b888913f939e2ac4f9ca9c7259472
sagar535 <070bct535@ioe.edu.np>
2021-06-16 23:31:01 +0545

    search for the skill

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Layout from "../components/Layout";
 import {useState, useEffect} from 'react'
-import SkillCard from "../components/SkillCard"
+import ListOfSkills from "../components/ListOfSkills";
 
 export default function Home() {
+    const [allSkills, setAllSkills] = useState([])
     const [skills, setSkills] = useState([])
 
     useEffect(() => {
@@ -11,18 +12,38 @@ export default function Home() {
                 .then(response => response.text())
                 .then(data => {
                     setSkills(JSON.parse(data))
+                    setAllSkills(JSON.parse(data))
                 })
         }
     })
 
+    const search = (e) => {
+        const filter = e.target.value
+        const filterRegExp = new RegExp(`${filter}`, 'i')
+        const skills = allSkills.filter((item) => item.match(filterRegExp))
+        setSkills(skills)
+    }
+
     return (
         <div>
             <Layout />
-            { (skills.length > 0) ? (<div className={'container'}>
-                <div className={'row'}>
-                    {skills.map((item, index) => (<SkillCard skill={item} key={index}/>))}
-                </div>
-            </div>) : (<h1>SKILLS LOADING</h1>) }
+            <div className={'container'}>
+                <form className="form-inline my-3 my-lg-0">
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                        onKeyUp={(e) => search(e)}
+                    />
+                </form>
+
+                { (skills.length > 0) ? (<div className={'container'}>
+
+                    <ListOfSkills skills={skills}/>
+                </div>) : (<h1>SKILLS LOADING</h1>) }
+            </div>
+
         </div>
       )
 }
