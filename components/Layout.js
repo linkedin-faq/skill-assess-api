9bdf922bcb15a1efc55ef7024544e666b4fe4968
Sagar Shah <070bct535@ioe.edu.np>
2022-01-04 22:04:05 +0545

    gives name to the functional components

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function () {
+export default function layout() {
     return (
         <>
             <Head>
--- components/ListOfSkills.js
+++ components/ListOfSkills.js
@@ -1,6 +1,6 @@
 import SkillCard from "./SkillCard";
 
-export default function (skills) {
+export default function listOfSkills(skills) {
     return (<div className={'row'}>
                 {skills.skills.map((item, index) => (<SkillCard skill={item} key={index}/>))}
             </div> )
