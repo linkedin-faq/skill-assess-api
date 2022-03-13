5b581ef986b2cca094ddf5a2a2ee22bea776cab6
Sagar Shah <070bct535@ioe.edu.np>
2022-03-13 23:05:48 +0545

    creates new guest user and saves associated token in the cookies

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,46 @@
 import Layout from "../components/Layout";
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import ListOfSkills from "../components/ListOfSkills";
 import {getAllSkillNames} from "../lib/skills";
+import faunadb, {query as q} from 'faunadb';
+import { useCookies } from "react-cookie";
+
+const client = new faunadb.Client({secret: process.env.NEXT_PUBLIC_FAUNA_GUEST_SECRET})
+
+// get all methods with spread operator
+const { Let, Select, Create, Collection, Tokens, Var, Update } = q
 
 export default function Home(props) {
     const { skillNames } = props;
     const [allSkills] = useState(skillNames);
     const [skills, setSkills] = useState(skillNames)
+
+    const [cookie, setCookie, removeCookie] = useCookies(["guetUserToken"])
+
+    useEffect(async () => {
+            if (cookie.guestUserToken === undefined) {
+                try {
+                    // make request to faunadb to create new token
+                    const response = await client.query(
+                            Let(
+                                {
+                                    // create a new guestUser
+                                    ref: Select("ref", Create(Collection("GuestUser"))),
+                                    token: Select("secret", Create(Tokens(), { instance: Var("ref") }))
+                                },
+                                Update(Var("ref"), { data: { token: Var("token") } })
+                            )
+                        )
+
+                    console.log(response)
+
+                    // save the token associated with guest User in the cookie
+                    setCookie("guestUserToken", response.data.token)
+                } catch(error) {
+                    console.log(error)
+                }
+        }
+    }, [])
 
 
     const search = (e) => {
